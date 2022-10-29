ee06c72002efab99baa4014b1236a99d50bb5a86
btatenov <btatenov@nurtelecom.kg>
2022-10-29 19:41:26 +0600

    Initial commit

---
+++ src/main/java/com/leemo/leemo/LeemoApplication.java
@@ -0,0 +1,13 @@
+package com.leemo.leemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leemo/leemo/LeemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leemo.leemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
