260fc5aa24f8c5af0198345e9ec7b0ecd96468d5
AkhtamIsPutin <Kuukilovekulpush@gmail.com>
2022-12-01 17:48:58 +0600

    AdminTasks

--- src/main/java/com/leemo/leemo/repo/TasksRepository.java
+++ src/main/java/com/leemo/leemo/repo/TasksRepository.java
@@ -1,9 +1,17 @@
 package com.leemo.leemo.repo;
 
 import com.leemo.leemo.entity.Tasks;
+import com.leemo.leemo.enums.Status;
+import com.leemo.leemo.enums.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface TasksRepository extends JpaRepository<Tasks,Long> {
+    List<Tasks> findAllByStatus(TaskStatus taskStatus);
+
+    Tasks findAllById (Long id);
 }
--- src/main/java/com/leemo/leemo/service/TaskService.java
+++ src/main/java/com/leemo/leemo/service/TaskService.java
@@ -3,6 +3,7 @@ package com.leemo.leemo.service;
 import com.leemo.leemo.entity.Tasks;
 import com.leemo.leemo.entity.UploadedFile;
 import com.leemo.leemo.entity.Users;
+import com.leemo.leemo.enums.Status;
 import com.leemo.leemo.enums.TaskStatus;
 import com.leemo.leemo.repo.FileUploadRepository;
 import com.leemo.leemo.repo.TasksRepository;
@@ -12,9 +13,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 
+import javax.websocket.OnClose;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
-
+import java.util.List;
 
 
 @Service
@@ -26,7 +30,7 @@ public class TaskService {
     @Autowired
     FileUploadRepository fileUploadRepository;
 
-    public void createTask(Tasks task, String username){
+    public void createTask(Tasks task, String username) {
         Users firstByEmail = repository.findFirstByEmail(username);
         task.setStatus(TaskStatus.ON_REVIEW);
         task.setCustomerId(firstByEmail.getId());
@@ -34,32 +38,36 @@ public class TaskService {
         tasksRepository.save(task);
     }
 
-    public Tasks findTask(Long id){
-    return this.tasksRepository.findById(id).orElse(null);
+    public Tasks findTask(Long id) {
+        return this.tasksRepository.findById(id).orElse(null);
     }
 
 
-    public void checkTask(Long id, TaskStatus taskStatus){
-        if (findTask(id) != null){
-        findTask(id).setStatus(taskStatus);
+    public void checkTask(Long id, TaskStatus taskStatus) {
+        if (findTask(id) != null) {
+            findTask(id).setStatus(taskStatus);
         }
     }
-    public void cancelTask(Long id){
-        if (findTask(id) != null){
+
+    public void cancelTask(Long id) {
+        if (findTask(id) != null) {
             findTask(id).setStatus(TaskStatus.CANCELED);
         }
     }
-    public Users findUser(Long id){
+
+    public Users findUser(Long id) {
         return this.repository.findById(id).orElse(null);
     }
-    public void chooseExecutor(Long userId, Long taskId){
-        if (findUser(userId)!=null){
-            if (findTask(taskId)!=null){
+
+    public void chooseExecutor(Long userId, Long taskId) {
+        if (findUser(userId) != null) {
+            if (findTask(taskId) != null) {
                 findTask(taskId).setExecutorId(userId);
                 findTask(taskId).setStatus(TaskStatus.IN_PROGRESS);
             }
         }
     }
+
     public void doneTask(Long userId, Long taskId) {
         if (findUser(userId) != null) {
             if (findTask(taskId) != null) {
@@ -67,6 +75,7 @@ public class TaskService {
             }
         }
     }
+
     public UploadedFile downloadFile(String fileId) {
         return fileUploadRepository.findFirstByFileId(fileId);
     }
@@ -79,7 +88,7 @@ public class TaskService {
             uploadedFile.setFileName(file.getOriginalFilename());
             uploadedFile.setTask(tasks);
             fileUploadRepository.save(uploadedFile);
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -88,4 +97,31 @@ public class TaskService {
         return fileUploadRepository.findByTaskId(taskId)
                 .orElse(new UploadedFile());
     }
+
+    public List<Tasks> getAllTaskAdmin() {
+        return tasksRepository.findAllByStatus(TaskStatus.ON_REVIEW);
+    }
+
+    public void updateTaskStatus(Long id, String taskStatus) {
+        Tasks tasks = tasksRepository.findAllById(id);
+        if (taskStatus.equals("ON_REVIEW")) {
+            tasks.setStatus(TaskStatus.ON_REVIEW);
+        } else if (taskStatus.equals("CANCELED")) {
+            tasks.setStatus(TaskStatus.CANCELED);
+        }else if (taskStatus.equals("PUBLISHED")) {
+            tasks.setStatus(TaskStatus.PUBLISHED);
+        }else if (taskStatus.equals("IN_PROGRESS")) {
+            tasks.setStatus(TaskStatus.IN_PROGRESS);
+        }else if (taskStatus.equals("DONE")) {
+            tasks.setStatus(TaskStatus.DONE);
+        }else if (taskStatus.equals("IN_ARGUE")) {
+            tasks.setStatus(TaskStatus.IN_ARGUE);
+        }else if (taskStatus.equals("RESOLVED")) {
+            tasks.setStatus(TaskStatus.RESOLVED);
+        }else if (taskStatus.equals("RETURNED")) {
+            tasks.setStatus(TaskStatus.RETURNED);
+        }
+        tasksRepository.save(tasks);
+    }
 }
+
