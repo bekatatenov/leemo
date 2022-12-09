050ab3e2a696eb5eeea083b2415ebc0fe0e67e04
Kuro107 <gotovo56@gmail.com>
2022-12-09 15:34:55 +0600

    createCandidates

--- src/main/java/com/leemo/leemo/service/CandidatesService.java
+++ src/main/java/com/leemo/leemo/service/CandidatesService.java
@@ -1,5 +1,6 @@
 package com.leemo.leemo.service;
 
+import com.leemo.leemo.entity.Candidates;
 import com.leemo.leemo.repo.CandidatesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,25 @@ public class CandidatesService {
     @Autowired
     CandidatesRepository candidatesRepository;
 
-//    public void respond(Long taskId){
-//        if (candidatesRepository.getCandidatesByTaskId() )
-//    }
+    public void createCandidates(Candidates candidates){
+        candidatesRepository.save(candidates);
+    }
+
+    public void respond(Long taskId, String email){
+      Candidates candidate = candidatesRepository.findByTaskId(taskId);
+        candidate.setExecutor(email);
+        candidatesRepository.save(candidate);
+    }
+
+    public Candidates getCandidates(Long id){
+        return candidatesRepository.findByTaskId(id);
+    }
+
+    public void responded(Long taskId, String username){
+        Candidates candidates = getCandidates(taskId);
+        if (candidates.getExecutor()!=null){
+            if (username.equals(candidates.getExecutor())) {
+            }
+        }
+    }
 }
